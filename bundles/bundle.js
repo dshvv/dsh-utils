5c808fb290d433d5babce51c6df4dfc4f3b700ec
丁少华 <dingshaohua@aegis-data.cn>
2020-12-31 17:49:53 +0800

    fix: 修复无法引用问题

---
+++ bundles/bundle.js
@@ -0,0 +1,56 @@
+/*
+ *@description: 查找数组的上一个或者下一个元素
+ *@author:  丁少华
+ *@date: 2020-12-31 11:11:12
+ */
+
+const getArrAdj = (param) => {
+    const { arr, type = 'next', currentIndex, currentItem, circular = true } = param;
+    // 如果没有传当前元素的index只传了item，我们找到item的index即可
+    if (!currentIndex && currentItem) {
+        currentIndex = arr.findIndex(item => item === currentItem);
+    }
+
+    if (type === 'next') {
+        // 如果到达边界
+        if (currentIndex && currentIndex === arr.length - 1) {
+            // 循环，则下一个是数组的起点元素
+            return circular?arr[0]: false;
+        } else {
+            return arr[currentIndex + 1]
+        }
+    } else {
+        // 如果到达边界
+        if (currentIndex === 0) {
+            // 循环，则下一个是数组的终点元素
+            return circular?arr[arr.length - 1]:false;
+        } else {
+            return arr[currentIndex - 1]
+        }
+    }
+};
+
+var arrayHelper = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    getArrAdj: getArrAdj
+});
+
+const getStorage = (key)=>{
+    const resStr = localStorage.getItem(key);
+    return JSON.parse(resStr);
+};
+
+var storageHelper = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    getStorage: getStorage
+});
+
+// 导入所有
+var index = {
+    ...arrayHelper,
+    ...storageHelper
+};
+
+export default index;
+export { getArrAdj, getStorage };
+//# sourceMappingURL=bundle.js.map
