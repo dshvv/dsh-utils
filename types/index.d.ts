ec8a75638b749ea2ef7ca3477253b3672e933128
丁少华 <dingshaohua@aegis-data.cn>
2021-01-02 23:41:08 +0800

    fix: 类型提示

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,5 @@
-// declare interface Window{
-//     dshUtils: dshUtils
-// }
 
-declare interface getArrAdjParams {
+interface getArrAdjParams {
 	arr: Array<any>; // 数组
 	type: string; // next/prev  上一个或者下一个
 	currentIndex: Number;// 当前激活的元素索引 与 currentItem组成可选
@@ -11,6 +8,9 @@ declare interface getArrAdjParams {
 }
 
 
-declare module dshUtils {
+declare module DshUtils {
 	function getArrAdj(params: getArrAdjParams): any
 }
+
+
+export default DshUtils;
