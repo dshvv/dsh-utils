e44fd5ed4ac1aa354870d906c2ad098fa729dd13
丁少华 <dingshaohua@aegis-data.cn>
2021-06-01 14:47:53 +0800

    fix: 加入判断json方法

---
+++ src/comp/other-helper.js
@@ -0,0 +1,24 @@
+// 判断是不是json字符串
+export const isJsonStr = (str) => {
+    if (typeof str == 'string') {
+        try {
+            var obj = JSON.parse(str);
+            if (typeof obj == 'object' && obj) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (e) {
+            console.log('error：' + str + '!!!' + e);
+            return false;
+        }
+    }
+};
+
+// 判断是不是json
+export const isJson = (data) => {
+    const typeofRes = typeof (data) == "object";
+    const toStringRes = Object.prototype.toString.call(data).toLowerCase() == "[object object]";
+    const isLen = !data?.length;
+    return typeofRes && toStringRes && isLen;
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import * as arrayHelper from './comp/array-helper.js';
 import * as storageHelper from './comp/storage-helper.js';
 import * as timeHelper from './comp/time-helper.js';
+import * as otherHelper from './comp/other-helper.js';
 
 // 按需导入
 export * from './comp/array-helper.js';
 export * from './comp/storage-helper.js';
 export * from './comp/time-helper.js';
+export * from './comp/other-helper.js';
 
 // 导入所有
 export default {
     ...arrayHelper,
     ...storageHelper,
-    ...timeHelper
+    ...timeHelper,
+    ...otherHelper
 }
